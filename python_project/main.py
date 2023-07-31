c7893beb538106e4198be1a0f0e03819a2625bc5
arunlsplk <arunpjmca@gmail.com>
2023-07-31 16:16:20 +0530

    updating

--- python_project/main.py
+++ python_project/main.py
@@ -1,8 +1,10 @@
-def Add(a, b):
-        return a + b
-        
-def SayHello():
-        print("sup world from srcmake")
+
+from sample import Add
+from sample import Sub
+from main_test import SayHello
 
 if __name__ == '__main__':
         SayHello()
+        Add(5,6)
+        Sub(237-37)
+
---
+++ python_project/sample.py
@@ -0,0 +1,9 @@
+def Add(a, b):
+        add=a+b
+        print("Addition value is:"+ add)
+        return add
+
+def Sub(a,b):
+        sub= a-b
+        print("substraction value is:"+ sub)
+        return sub
