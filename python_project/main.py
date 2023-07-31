6ce656618b2254d3deae389815d4d87965dcb4dd
arunlsplk <arunpjmca@gmail.com>
2023-07-31 14:13:38 +0530

    updating

--- python_project/main.py
+++ python_project/main.py
@@ -1,8 +1,5 @@
-def Add(a, b):
-        return a + b
-        
 def SayHello():
-        print("sup world from srcmake")
+        print("Hello World!!")
 
 if __name__ == '__main__':
         SayHello()
