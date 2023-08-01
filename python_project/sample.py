1c822ca410a6d627215878463746856a726ea161
arunlsplk <arunpjmca@gmail.com>
2023-08-01 10:49:29 +0530

    updating

--- python_project/sample.py
+++ python_project/sample.py
@@ -1,9 +1,9 @@
 def Add(a, b):
         add=a+b
-        print("Addition value is:"+ add)
+        print("Addition value is:"+ str(add))
         return add
 
 def Sub(a,b):
         sub= a-b
-        print("substraction value is:"+ sub)
+        print("substraction value is:"+ str(sub))
         return sub
